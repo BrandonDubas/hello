0e1b31a8af78c34b9bd2d6a4216300cf06bffdd6
BrandonDubas <brandoncdubas@gmail.com>
2024-08-29 12:57:04 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Brandon Dubas
+ *         bdubas2@unl.edu
+ * Date: 2024/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brandon Dubas!\n");
+  printf("My major is Computer Engineering");
+
+  return 0;
+}
